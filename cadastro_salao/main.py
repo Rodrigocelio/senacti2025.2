6626feb1a643e327d8f1ac002aa9b3e634a20a7a
rodrigocelio <rodrigocelio@live.com>
2025-09-11 11:31:28 -0300

    funcao mostrar_menu criada

---
+++ cadastro_salao/main.py
@@ -0,0 +1,9 @@
+# funcao para exibir o menu
+def mostrar_menu():
+    print("\n=== Sistema de Cadastro de Clientes - Barbearia ===")
+    print("1. Cadastrar cliente")
+    print("2. Listar clientes")
+    print("3. Buscar cliente por nome")
+    print("4. Sair")
+
+# função para declarar variáveis
