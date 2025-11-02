318d2e31cffda1e247d329e0ce4ae4309dac2051
abraao <abraao@kali-abraao.dev>
2025-11-02 01:08:13 -0300

    Adicinado função para cadastrar adm

--- cadastro_salao/teste.py
+++ cadastro_salao/teste.py
@@ -1,33 +1,49 @@
 import getpass
+import os
 
 
-def logar(user, password):
+ARQ_CREDENCIAIS = "./senacti2025.2/cadastro_salao/credenciais.txt"
+
+
+def cadastrar_adm():
+    """Cadastra um novo administrador no sistema."""
+    if os.path.exists(ARQ_CREDENCIAIS):
+        with open(ARQ_CREDENCIAIS, "a") as credenciais:
+            usuario, senha = solicitar_login()
+            credenciais.write(f"-{usuario}\n")
+            credenciais.write(f":{senha}\n\n")
+    else:
+        raise Exception("Arquivo de credenciais não encontrado!")
+
+
+def logar(usuario, senha):
     """Verifica se o usuário é um dos adminitradores do sistema. Os valores de 
     login são armazenados em um dicionário que é consultado para validar o 
     acesso.
     """ 
-    adm = {'usuario': '', 
-           'senha': ''}
+    credenciais = buscar_credenciais_registradas()
+    print(credenciais)
+    # Valida a credencial informada pelo usuário.
+    for i in range(len(credenciais['usuarios'])):
+        if usuario == credenciais['usuarios'][i] and senha == credenciais['senhas'][i]:
+            print("Ok")
+            return 1
+    return 0
 
+
+def buscar_credenciais_registradas():
+    """Busca as credenciais armazenadas no arquivo."""
+    adm = {'usuarios': [], 'senhas': []}
     try:
-        with open("./senacti2025.2/cadastro_salao/credenciais.txt") as credenciais:
+        with open(ARQ_CREDENCIAIS) as credenciais:
             for linha in credenciais:
-                # captura o nome de usuário.
                 if linha.startswith("-"):
-                    usuario = linha.removeprefix("-")
-                    usuario = usuario.removesuffix("\n")
-                    adm["usuario"] = usuario
-                # captura a senha.
+                    adm['usuarios'].append(linha.removeprefix("-").removesuffix("\n"))
                 if linha.startswith(":"):
-                    senha = linha.removeprefix(":")
-                    senha = senha.removesuffix("\n")
-                    adm["senha"] = senha
+                    adm['senhas'].append(linha.removeprefix(":").removesuffix("\n"))
     except OSError as erro:
         print(erro)
-    else:
-        # Valida a credencial informada pelo usuário.
-        if user == adm['usuario'] and password == adm['senha']:
-                print("OK")
+    return adm
 
 
 def solicitar_login():
@@ -36,7 +52,9 @@ def solicitar_login():
     """
     usuario = input("Usuário: ")
     senha = getpass.getpass("Senha: ", stream=None)
-    logar(usuario, senha)
+    return usuario, senha
 
-
-solicitar_login()
+# O simbolo de * serve para indicar que uma tupla será
+# desconpactada.
+logar(*solicitar_login())
+#cadastrar_adm()
